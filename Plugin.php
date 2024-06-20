5aea87a29083fd9c0e5633359b61cd252efb951d
Gugi Gustaman <gugi.gustaman.15@gmail.com>
2024-06-20 18:16:54 +0200

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,19 @@
+<?php namespace Kingu\Team;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    /**
+     * @var array Plugin dependencies
+     */
+    public $require = ['Kingu.Main'];
+
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
